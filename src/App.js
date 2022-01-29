5a68b03c557d00e482dd735e3cf92c27ed33d13c
pawanlaksahan <pawanlaksahan@email.com>
2022-01-30 02:54:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const[inputText,setInputText]=useState("");
+  const[Todos,setTodos]=useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Form inputText={inputText} todos={Todos} setTodos={setTodos} setInputText={setInputText}/>
+      <TodoList todos={Todos} setTodos={setTodos}/>
     </div>
+    
   );
 }
 
